c3a173f90a69e49717cd23b6ef858af6b16654cc
Reaksaa <104069671+Reaksaa@users.noreply.github.com>
2022-12-14 10:43:25 +0700

    change language on HomeWorkShowcase ( heading )

--- frontend/screens/home/HomeWorkShowcase.tsx
+++ frontend/screens/home/HomeWorkShowcase.tsx
@@ -1,11 +1,13 @@
 import { WorkShowcasesResponse } from 'api/interface';
 import { Heading } from 'components/Heading';
 import { WorkShowcase } from 'components/WorkShowcase/WorkShowcase';
+import { useTranslation } from 'react-i18next';
 
 export const HomeWorkShowcase: React.FC<{ data: WorkShowcasesResponse }> = ({ data }) => {
+  const { t } = useTranslation();
   return (
     <div className="container py-4 md:px-8">
-      <Heading text={'Works Showcase'} link="/works" />
+      <Heading text={t('works-showcase')} link="/works" />
       <WorkShowcase data={data} />
     </div>
   );
